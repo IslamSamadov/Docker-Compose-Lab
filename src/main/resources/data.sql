d6cb6532ec3a66eb73eedbd59d77e464c5ef9e77
IslamSamadov <islamsamedov52@gmail.com>
2026-05-08 17:39:07 +0400

    finish lab

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO persons (name) VALUES ('Alice');
+INSERT INTO persons (name) VALUES ('Bob');
+INSERT INTO persons (name) VALUES ('Charlie');
+INSERT INTO persons (name) VALUES ('Diana');
+INSERT INTO persons (name) VALUES ('Eduardo');
